37af003b6c8292957f7629f0a21bfd0fd17c6c92
Nguyen Nhat Anh <anhnnbhaf190127@fpt.edu.vn>
2022-08-13 21:39:27 +0700

    da duoc chinh by nhat anh

--- resources/views/edituser.blade.php
+++ resources/views/edituser.blade.php
@@ -3,15 +3,13 @@
 @section('main-content')
 <form role ="form" action="" method="post">
     @csrf       
-        <label>User name</label>
+        <label>User Name</label>
         <input class="form-control" name="username" value ="{{$user->username}}">
         <label>Email</label>
         <input class="form-control" name="email" value ="{{$user->email}}">        
     <button type="submit" class="btn btn-success">Submit Button</button>
     <button type="reset" class="btn btn-primary">Reset Button</button>
 </form>
-
-
 
 
 @endsection
